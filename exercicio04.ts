7d98d6f9cf75b708206e78c7dd609d05719a4d9e
Esther Freixo <freixo.esther@gmail.com>
2025-08-03 21:39:03 -0300

    add: exercicio 04

---
+++ exercicio04.ts
@@ -0,0 +1,15 @@
+type Usuario = {
+    nome: string,
+    email: string,
+    exibirInfo: () => string
+}
+
+const user = {
+    nome: "Esther",
+    email: "esther@gmail.com",
+    exibirInfo() {
+        return `Nome: ${this.nome} - Email: ${this.email}`;
+      }
+}
+
+console.log(user.exibirInfo());
