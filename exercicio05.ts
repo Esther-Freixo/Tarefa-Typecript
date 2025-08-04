faa933225d07264b1f8478d3a7b360cc2a2867d5
Esther Freixo <freixo.esther@gmail.com>
2025-08-03 21:41:33 -0300

    add: exercicio 05

---
+++ exercicio05.ts
@@ -0,0 +1,19 @@
+type Genero = 'masculino' | 'feminino';
+
+interface IPessoa {
+    nome: string;
+    idade: number;
+    genero: Genero;
+}
+
+function apresentarPessoa(pessoa: IPessoa): string {
+    return `${pessoa.nome} tem ${pessoa.idade} anos e se identifica como ${pessoa.genero}.`;
+}
+
+let pessoa: IPessoa = {
+    nome: "Esther",
+    idade: 25,
+    genero: 'feminino'
+};
+
+console.log(apresentarPessoa(pessoa));
